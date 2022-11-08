1f9e4f01e54ae8e77111a71f0e122434505d94a4
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-08 23:44:52 +0600

    react-photo-view set up

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServiceCard = ({service}) => {
     const {_id ,img, price, title, description} = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl mt-8">
-            <figure><img src={img} alt="Shoes" className='p-5 rounded-3xl'/></figure>
+            {/* <figure><img src={img} alt="Shoes" className='p-5 rounded-3xl'/></figure> */}
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <img src={img} alt="img" className='p-5 rounded-3xl'/>
+                </PhotoView>
+            </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-xl text-orange-600 font-semibold'>Price: {price}</p>
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -6,13 +6,15 @@ import error from '../../../assets/img/error.gif';
 
 const NotFound = () => {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src={error} alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-                <div className="card-actions">
-                <Link className='btn btn-secondary' to={`/home`}><button className="text-white text-xl inline-flex justify-center items-center">see all <BsArrowRight /> </button></Link>
+        <div className='grid gap-6 grid-cols-1 md:grid-cols-1 lg:grid-cols-1 mb-10 mt-5'>
+            <div className="card w-96 bg-base-100 shadow-xl items-center justify-center text-center">
+                <figure className="px-10 pt-10">
+                    <img src={error} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <div className="card-actions">
+                    <Link className='btn btn-secondary' to='/'><button className="text-white text-xl inline-flex justify-center items-center">Go Too Home <BsArrowRight /> </button></Link>
+                    </div>
                 </div>
             </div>
         </div>
